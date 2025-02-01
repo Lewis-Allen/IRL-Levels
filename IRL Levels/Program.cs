810c403004265dfc7c6ec41f727c6aec559db769
Lewis-Allen <lewisn11@hotmail.co.uk>
2025-02-01 12:46:10 +0000

    Clipboard Shenanigains

--- IRL Levels/Program.cs
+++ IRL Levels/Program.cs
@@ -1,5 +1,6 @@
 using Blazored.LocalStorage;
 using IRL_Levels;
+using IRL_Levels.Services.Clipboard;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -8,6 +9,7 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped<IClipboardService, ClipboardService>();
 builder.Services.AddBlazoredLocalStorage();
 
 await builder.Build().RunAsync();
---
+++ IRL Levels/Services/Clipboard/ClipboardService.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.JSInterop;
+
+namespace IRL_Levels.Services.Clipboard;
+
+public interface IClipboardService
+{
+    Task CopyToClipboard(string text);
+}
+
+public class ClipboardService(IJSRuntime _jsInteropt) : IClipboardService
+{
+    public async Task CopyToClipboard(string text)
+    {
+        await _jsInteropt.InvokeVoidAsync("navigator.clipboard.writeText", text);
+    }
+}
